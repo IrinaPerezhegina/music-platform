0157f7617c3c8046c21d648bce59d9389f3b00bb
IrinaPerezhegina <perezeginaiv@mail.ru>
2025-01-09 09:58:28 +0500

    created Schemas for Track and Comment

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Track, TrackDocument } from './schemas/track.schema';
+import { Model } from 'mongoose';
+import { Comment, CommentDocument } from './schemas/comment.schema';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class TrackService {
-  async create() {}
+  constructor(
+    @InjectModel(Track.name) private trackModel: Model<TrackDocument>,
+    @InjectModel(Comment.name)
+    private commentModel: Model<CommentDocument>,
+  ) {}
+  async create() {
+    const track = await this.trackModel.create({});
+  }
   async getAll() {}
   async getOne() {}
   async delete() {}
