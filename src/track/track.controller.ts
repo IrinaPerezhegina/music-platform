060f63cdf52724ffa76097427af8accca80a7417
IrinaPerezhegina <perezeginaiv@mail.ru>
2025-01-13 13:49:49 +0500

    add FileService, FileModule

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,15 +1,34 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
 import { TrackService } from './track.service';
 import { CreateTrackDto } from './dto/create-track.dto';
 import { ObjectId } from 'mongoose';
 import { CreateCommentDto } from './dto/create-comment.dto';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
 
 @Controller('/tracks')
 export class TrackController {
   constructor(private trackService: TrackService) {}
+
   @Post()
-  create(@Body() dto: CreateTrackDto) {
-    return this.trackService.create(dto);
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'picture', maxCount: 1 },
+      { name: 'audio', maxCount: 1 },
+    ]),
+  )
+  create(@UploadedFiles() files, @Body() dto: CreateTrackDto) {
+    const { picture, audio } = files;
+
+    return this.trackService.create(dto, picture[0], audio[0]);
   }
 
   @Get()
@@ -31,4 +50,5 @@ export class TrackController {
   addComment(@Body() dto: CreateCommentDto) {
     return this.trackService.addComment(dto);
   }
+  // "34:08"
 }
