c26b6c0ca488b75410ed3eb4aa467ba192c05309
IrinaPerezhegina <perezeginaiv@mail.ru>
2025-01-18 18:30:58 +0500

    added dowland files

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { FileService } from './file/file.service';
       'mongodb://ue6cq6qjzkpxiuzxlun9:NOF77cpXhc71Z1GxXxaR@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bclirkbgnf25vjs?replicaSet=rs0',
     ),
     TrackModule,
-    FileService,
+    // FileService,
   ],
 })
 export class AppModule {}
