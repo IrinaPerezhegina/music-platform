6cbdc9b12633f115e1972e3d91cb6badd13cf195
IrinaPerezhegina <perezeginaiv@mail.ru>
2025-01-19 13:00:36 +0500

    added distribution of static

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TrackModule } from './track/track.module';
 import { MongooseModule } from '@nestjs/mongoose';
-import { FileService } from './file/file.service';
+import { FileModule } from './file/file.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
     MongooseModule.forRoot(
       'mongodb://ue6cq6qjzkpxiuzxlun9:NOF77cpXhc71Z1GxXxaR@n1-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017,n2-c2-mongodb-clevercloud-customers.services.clever-cloud.com:27017/bclirkbgnf25vjs?replicaSet=rs0',
     ),
     TrackModule,
-    // FileService,
+    FileModule,
+    // 48:09
   ],
 })
 export class AppModule {}
