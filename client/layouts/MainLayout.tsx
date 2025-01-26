ab4754f39f161c25ac5d7c5569ba5df51ed9cddc
IrinaPerezhegina <perezeginaiv@mail.ru>
2025-01-26 20:01:11 +0500

    added MainLayout

---
+++ client/layouts/MainLayout.tsx
@@ -0,0 +1,17 @@
+import Navbar from '@/components/Navbar'
+import { Container } from '@mui/material'
+import { ReactNode } from 'react'
+
+type MainLayoutProps = {
+  children: ReactNode
+}
+const MainLayout = ({ children }: MainLayoutProps) => {
+  return (
+    <>
+      <Navbar />
+      <Container style={{ margin: '90px 0' }}>{children}</Container>
+    </>
+  )
+}
+
+export default MainLayout
---
+++ client/pages/tracks/index.tsx
@@ -0,0 +1,11 @@
+import MainLayout from '@/layouts/MainLayout'
+
+const Index = () => {
+  return (
+    <MainLayout>
+      <div>Список треков</div>
+    </MainLayout>
+  )
+}
+
+export default Index
