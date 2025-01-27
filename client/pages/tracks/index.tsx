d440a37ff7e8faee525d9d9bf1d03febf184e10f
IrinaPerezhegina <perezeginaiv@mail.ru>
2025-01-27 09:57:40 +0500

    added CreatePage

--- client/pages/tracks/index.tsx
+++ client/pages/tracks/index.tsx
@@ -1,9 +1,56 @@
 import MainLayout from '@/layouts/MainLayout'
+import { ITtrack } from '@/types/track'
+import { Box, Button, Card, Grid2 } from '@mui/material'
+import { useRouter } from 'next/router'
 
 const Index = () => {
+  const router = useRouter()
+  const tracks: ITtrack[] = [
+    {
+      _id: '1',
+      name: 'Трек 1',
+      artist: 'Исполнитель 1',
+      text: 'Какой-то текст',
+      listens: 5,
+      comments: [],
+      picture: '',
+      audio: '',
+    },
+    {
+      _id: '2',
+      name: 'Трек 2',
+      artist: 'Исполнитель 2',
+      text: 'Какой-то текст',
+      listens: 2,
+      comments: [],
+      picture: '',
+      audio: '',
+    },
+    {
+      _id: '3',
+      name: 'Трек 3',
+      artist: 'Исполнитель 3',
+      text: 'Какой-то текст',
+      listens: 3,
+      comments: [],
+      picture: '',
+      audio: '',
+    },
+  ]
   return (
     <MainLayout>
-      <div>Список треков</div>
+      <Grid2 container justifyContent="center">
+        <Card style={{ width: '900px' }}>
+          <Box p={3}>
+            <Grid2 container justifyContent="space-between">
+              <h1>Cписок треков</h1>
+              <Button onClick={() => router.push('/tracks/create')}>
+                Загрузить
+              </Button>
+            </Grid2>
+          </Box>
+        </Card>
+      </Grid2>
     </MainLayout>
   )
 }
