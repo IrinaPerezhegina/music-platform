8560ce3975a2e8bf8584cf6047b161f1f7bb0e04
IrinaPerezhegina <perezeginaiv@mail.ru>
2025-01-29 10:15:02 +0500

    add TrackPage

--- client/components/TrackItem.tsx
+++ client/components/TrackItem.tsx
@@ -1,22 +1,28 @@
 import { ITtrack } from '@/types/track'
-import { Pause, PlayArrow } from '@mui/icons-material'
+import { Delete, Pause, PlayArrow } from '@mui/icons-material'
 import { Card, Grid2, IconButton } from '@mui/material'
+import { useRouter } from 'next/router'
 import styles from "../styles/TrackItem.module.scss"
 type TrackItemProps = {
   track: ITtrack
   active?: boolean
 }
 const TrackItem = ({ track, active = false }: TrackItemProps) => {
-  return <Card className={styles.track}>
-    <IconButton>
-        {active?<PlayArrow/>:<Pause/>}
+ const router=useRouter()
+ return <Card className={styles.track} onClick={()=>router.push('/tracks/'+track._id)}>
+    <IconButton onClick={e=>e.stopPropagation()}>
+        {!active?<PlayArrow/>:<Pause/>}
     </IconButton>
     <img width={70} height={70} src={track.picture}/>
     <Grid2 container direction={'column'} style={{width:200, margin:'0 20px'}}>
         <div >{track.name}</div>
         <div style={{fontSize:12, color:'gray'}}>{track.artist}</div>
     </Grid2>
+    {active && <div >02:42/03:23</div> }
+    <IconButton onClick={e=>e.stopPropagation()} style={{marginLeft:'auto'}}>
+      <Delete/>
+    </IconButton>
     </Card>
 }
-// 1:15:18
+
 export default TrackItem
---
+++ client/pages/tracks/[id].tsx
@@ -0,0 +1,52 @@
+import MainLayout from '@/layouts/MainLayout'
+import { ITtrack } from '@/types/track'
+import { Button, Grid2, TextField } from '@mui/material'
+import { useRouter } from 'next/router'
+
+const TrackPage = () => {
+ const router=useRouter()
+const track:ITtrack= {
+    _id: '1',
+    name: 'Трек 1',
+    artist: 'Исполнитель 1',
+    text: 'Какой-то текст',
+    listens: 5,
+    comments: [],
+    picture: 'https://avatars.mds.yandex.net/i?id=7809e4eae60352f45484a9eff53c962b64fd19b707b5f0ba-12938349-images-thumbs&n=13',
+    audio: '',
+  }
+ return (
+    <MainLayout >
+      <Button variant='outlined' style={{fontSize:32}} onClick={()=>router.push('/tracks')}>
+        К списку
+      </Button>
+      <Grid2 container style={{margin:'20px 0'}}>
+        <img src={track.picture} width={200} height={200} />
+        <div className="">
+            <h1>Название трека - {track.name}</h1>
+            <h1>Исполнитель - {track.artist}</h1>
+            <h1>Прослушиваний - {track.listens}</h1>
+        </div>
+      </Grid2>
+      <h1>Слова в треке</h1>
+     <p> {track.text}</p>
+    <h1>Комментарии</h1>
+    <Grid2>
+
+         <TextField label={'Ваше имя'}  fullWidth/>
+         <TextField label={'Комментарий'}  fullWidth multiline rows={4}/>
+    <Button>Отправить</Button>
+    </Grid2>
+    <div>
+        {track.comments.map((comment)=>(
+            <div >
+              <div >Автор - {comment.username}</div>
+                <div>Комментарий - {comment.text}</div>
+            </div>
+        ))}
+    </div>
+    </MainLayout>
+ )
+}
+
+export default TrackPage
