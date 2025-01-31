87aaba1037582b80e2e7d8750ea69d6ac842ffbb
IrinaPerezhegina <perezeginaiv@mail.ru>
2025-01-31 14:31:53 +0500

    added CreatePage

--- client/pages/tracks/create.tsx
+++ client/pages/tracks/create.tsx
@@ -1,12 +1,41 @@
 import StepWrapper from '@/components/StepWrapper'
 import MainLayout from '@/layouts/MainLayout'
+import { Button, Grid2 } from '@mui/material'
+import { useState } from 'react'
 
 const Create = () => {
+  const [activeStep, setActiveStep]=useState(0)
+  const next=()=>{
+    if (activeStep!==2) {
+    setActiveStep((prev)=>prev+1)
+    }
+  }
+  const back=()=>{
+      setActiveStep((prev)=>prev-1)
+  
+  
+  }
   return (
     <MainLayout>
-     <StepWrapper activeStep={0}>
-      <h1>Загрузка трека</h1>
+     <StepWrapper activeStep={activeStep}>
+      {activeStep===0 &&
+        <h1>STEP 1</h1>
+        }
+        {activeStep===1 &&
+        <h1>STEP 2</h1>
+        }
+        {activeStep===2 &&
+        <h1>STEP 3</h1>
+        }
      </StepWrapper>
+     <Grid2 container justifyContent={'space-between'}>
+      <Button disabled={activeStep===0}  onClick={back}>
+        Назад
+      </Button>
+      <Button  onClick={next}>
+        Далее
+      </Button>
+    </Grid2>
     </MainLayout>
   )
 }
